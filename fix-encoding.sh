3644233872c30541e618c66ca62603d2280dfbdb
Аббас Гусенов <gusenov@live.ru>
2018-05-27 15:19:48 +0600

    bash-скрипт для смены кодировки .h и .c файлов с windows-1251 на utf-8.

---
+++ fix-encoding.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#set -x  # echo on
+
+convert_files_encoding_sh="~/repo/lang/sh/convert-files-encoding-sh/convert-files-encoding.sh"
+eval convert_files_encoding_sh=$convert_files_encoding_sh
+
+target_project_dir="$(pwd)/"  # should include a trailing slash.
+
+"$convert_files_encoding_sh" \
+    -p="$target_project_dir" \
+    -t="*.h *.c" \
+    -i="windows-1251" \
+    -o="utf-8"
+
+# Предполагается, что все файлы в папке target_project_dir
+# либо в кодировке utf-8, либо в кодировке windows-1251.
+

a905681e09f0d5d6e99cd4bf46755848998b3713
Аббас Гусенов <gusenov@live.ru>
2018-05-27 15:20:57 +0600

    Смена кодировки некоторых .c файлов с windows-1251 на utf-8.
    recode windows-1251..utf-8 binary-data/rnd.c
    recode windows-1251..utf-8 tomtit/lab10/lab10-var12/main.c
    recode windows-1251..utf-8 tomtit/lab10/lab10-var15/main.c
    recode windows-1251..utf-8 tomtit/lab05/lab05-var12/main.c
    recode windows-1251..utf-8 tomtit/lab05/lab05-var15/main.c
    recode windows-1251..utf-8 tomtit/lab07/lab07-var15/main.c
    recode windows-1251..utf-8 tomtit/lab07/lab07-var12/main.c
    recode windows-1251..utf-8 tomtit/lab08/lab08-var15/main.c
    recode windows-1251..utf-8 tomtit/lab08/lab08-var12/main.c
    recode windows-1251..utf-8 tomtit/lab09/lab09-var12/main.c
    recode windows-1251..utf-8 tomtit/lab09/lab09-var15/main.c
    recode windows-1251..utf-8 tomtit/lab06/lab06-var15/main.c
    recode windows-1251..utf-8 tomtit/lab06/lab06-var12/main.c
    recode windows-1251..utf-8 replace-symbolic-constants/replace-symbolic-constants.c
    recode windows-1251..utf-8 cube-root/cuberoot.c

--- tomtit/lab05/lab05-var12/main.c
+++ tomtit/lab05/lab05-var12/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Процедура для ввода данных пользователем:
+// РџСЂРѕС†РµРґСѓСЂР° РґР»СЏ РІРІРѕРґР° РґР°РЅРЅС‹С… РїРѕР»СЊР·РѕРІР°С‚РµР»РµРј:
 void input(double *a, double *b, double *c, double *x0, double *xk, double *deltax) {
 	printf("a -> "); scanf("%lf", a);
 	printf("b -> "); scanf("%lf", b);
@@ -12,7 +12,7 @@ void input(double *a, double *b, double *c, double *x0, double *xk, double *delt
 	printf("deltax -> "); scanf("%lf", deltax);
 }
 
-// Функция для вычисления значения зависимого от x, a, b и c:
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ РІС‹С‡РёСЃР»РµРЅРёСЏ Р·РЅР°С‡РµРЅРёСЏ Р·Р°РІРёСЃРёРјРѕРіРѕ РѕС‚ x, a, b Рё c:
 double f(double x, double a, double b, double c) {
 	if (x < 0.6 && b + c != 0) {
 		return a * pow(x, 2.0) + pow(b, 2.0) + c;
@@ -29,11 +29,11 @@ double f(double x, double a, double b, double c) {
 int main(int argc, char *argv[]) {
 	double a, b, c, x0, x, xk, deltax;
 	
-	input(&a, &b, &c, &x0, &xk, &deltax);  // ввод данных.
+	input(&a, &b, &c, &x0, &xk, &deltax);  // РІРІРѕРґ РґР°РЅРЅС‹С….
 	
-	printf("%24s %24s\n", "x", "F(x, a, b, c)");  // вывод заголовка таблицы.
+	printf("%24s %24s\n", "x", "F(x, a, b, c)");  // РІС‹РІРѕРґ Р·Р°РіРѕР»РѕРІРєР° С‚Р°Р±Р»РёС†С‹.
 	
-	// Вычисление и вывод значений функции на диапазоне от x0 до xk с шагом deltax:
+	// Р’С‹С‡РёСЃР»РµРЅРёРµ Рё РІС‹РІРѕРґ Р·РЅР°С‡РµРЅРёР№ С„СѓРЅРєС†РёРё РЅР° РґРёР°РїР°Р·РѕРЅРµ РѕС‚ x0 РґРѕ xk СЃ С€Р°РіРѕРј deltax:
 	x = x0;
 	while (x <= xk) {
 		printf("%24.2lf %24.2lf\n", x, f(x, a, b, c));
